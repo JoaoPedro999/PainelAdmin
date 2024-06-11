a322516975d381f24ddddc8fc0481fd5db1ce87d
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-06-11 18:17:34 -0300

    Co-authored-by: CaioFelps007 <CaioFelps007@users.noreply.github.com> Co-authored-by: Guilherme Veiga Pedromilo <GuilhermeVeigaPedromilo@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const mysql = require('mysql2');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
-// use `prisma` in your application to read and write data in your DB
 
 const app = express();
 
@@ -19,22 +17,6 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false } // Set secure: true if using https
 }));
-
-// Configurar a conexão com o banco de dados MySQL
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-    throw err;
-  }
-  console.log('Conexão com o banco de dados MySQL estabelecida.');
-});
 
 // Configurar o diretório de views e o motor de views
 app.set('views', path.join(__dirname, 'views'));
@@ -54,65 +36,22 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('pages/index');
-}); 
+});
+
+app.get('/dashboard', (req, res) => {
+  res.render('pages/dashboard');
+});
 
 app.get('/login', (req, res) => {
-  res.render('pages/login'); // Renders views/login.ejs
+  res.render('pages/login'); 
 });
 
+app.get('/registro', (req, res) => {
+  res.render('pages/register'); 
+});
 
 app.get('/tables', (req, res) => {
-  res.render('pages/tables'); // Renders views/login.ejs
-});
-
-// Rota para processar o formulário de login
-app.post('/login', (req, res) => {
-  const { username, password, cpf } = req.body;
-
-  const query = 'SELECT * FROM users WHERE username = ? AND password = SHA1(?) AND cpf = ?';
-
-  db.query(query, [username, password, cpf], (err, results) => {
-    if (err) throw err;
-
-    if (results.length > 0) {
-      // Autenticação bem-sucedida
-      req.session.loggedin = true;
-      req.session.name = username;
-
-      // Verifique o tipo de usuário
-      const tipoUsuario = results[0].tipo;
-
-      req.session.tipoUsuario = tipoUsuario
-
-      if (tipoUsuario === 'user') {
-        console.log("Usuario Logado");
-        res.redirect('/agendamento')
-        req.session.loggedin = true;
-        req.session.name = username;
-      } else if (tipoUsuario === 'Medico') {
-        console.log("Usuario Logado");
-        res.redirect('/medicopage')
-        req.session.loggedin = true;
-        req.session.name = username;
-      } else if (tipoUsuario === 'Gestor') {
-        console.log("Usuario Logado");
-        res.redirect('/gestorpage')
-        req.session.loggedin = true;
-        req.session.name = username;
-      } else if (tipoUsuario === 'Administrador') {
-        console.log("Usuario Logado");
-        res.redirect('/indexadmin')
-        req.session.loggedin = true;
-        req.session.name = username;
-      } else {
-        // Tratamento para outros tipos de usuário ou tipo desconhecido
-        res.send('Tipo de usuário desconhecido. <a href="/">Tente novamente</a>');
-      }
-    } else {
-      // Credenciais incorretas
-      res.send('<h1>Credenciais Incorretas');
-    }
-  });
+  res.render('pages/tables'); 
 });
 
 // Rota para fazer logout
